9a277231e46f815d12ab4aa1340398963ca9d50e
adminpro <minhchien201186@yahoo.com.vn>
2013-09-09 18:23:23 +0700

    Implement function StringToBase64

---
+++ MVC_DesignPattern/Controllers/Base64Controller.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_DesignPattern.ViewModel;
+
+namespace MVC_DesignPattern.Controllers
+{
+    public class Base64Controller : Controller
+    {
+        //
+        // GET: /Base64/
+        [HttpGet]
+        public ActionResult Index()
+        {
+            VMBase64 vmIndex = new VMBase64();
+            ViewBag.Base64ConvertType = EnumToSelectList<Base64ConvertType, byte>(vmIndex.Type);
+            return View(vmIndex);
+        }
+        [HttpPost]
+        public ActionResult Index(VMBase64 vmIndex)
+        {
+            switch (vmIndex.Type)
+            {
+                case Base64ConvertType.StringToBase64:
+                    return RedirectToAction("Encode", vmIndex);
+                case Base64ConvertType.Base64ToString:
+                    return RedirectToAction("Decode", vmIndex);
+                default:
+                    return View(vmIndex);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Encode()
+        {
+            return View(new VMBase64());
+        }
+        [HttpPost]
+        public ActionResult Encode(VMBase64 vmEncode)
+        {
+            ModelState.Remove("Destination");
+            vmEncode.Destination = Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(vmEncode.Source));
+            return View(vmEncode);
+        }
+        [HttpGet]
+        public ActionResult Decode()
+        {
+            VMBase64 vmEncode = new VMBase64
+            {
+                Type = Base64ConvertType.Base64ToString
+            };
+            return View(vmEncode);
+        }
+        [HttpPost]
+        public ActionResult Decode(VMBase64 vmDecode)
+        {
+            ModelState.Remove("Destination");
+            vmDecode.Destination = System.Text.Encoding.UTF8.GetString(Convert.FromBase64String(vmDecode.Source));
+            return View(vmDecode);
+        }
+
+        private SelectList EnumToSelectList<T, dataType>(T objVal)
+        {
+            var listEnumValues = (from T c in Enum.GetValues(typeof(T))
+                                  select new { 
+                                      Key = c,
+                                      Value = c.ToString()
+                                  });
+            return new SelectList(listEnumValues, "Key", "Value", objVal);
+        }
+    }
+}
---
+++ MVC_DesignPattern/ViewModel/VMBase64.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVC_DesignPattern.ViewModel
+{
+    [ValidateInput(false)]
+    public class VMBase64
+    {
+        public VMBase64()
+        {
+            this.Type = Base64ConvertType.StringToBase64;
+        }
+        [AllowHtml]
+        [DataType(DataType.MultilineText)]
+        public string Source { get; set; }
+        [AllowHtml]
+        [DataType(DataType.MultilineText)]
+        public string Destination { get; set; }
+        public Base64ConvertType Type { get; set; }
+    }
+
+    public enum Base64ConvertType : byte
+    {
+        StringToBase64 = 1,
+        Base64ToString = 2
+    }
+}
